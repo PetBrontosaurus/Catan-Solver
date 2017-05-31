35b508b23c580bb8a080d0e6f87a02cd5e24287c
PetBrontosaurus <PetBrontosaurus@users.noreply.github.com>
2017-05-31 02:57:37 -0400

    Alternative attempt at sheep port.
    Still requires some testing - unification seems to be the right way to 'append' to get multiple 'solutions' at that point?

--- Catan.pl
+++ Catan.pl
@@ -1,60 +1,76 @@
-[ stone,
-  stone,
-  brick,
-  wood,
-  wheat,
-  sheep,
-  stone,
-  stone,
-  brick,
-  wood,
-  wheat,
-  sheep,
-  stone,
-  stone,
-  brick,
-  wood,
-  wheat,
-  sheep].
+card(stone).
+card(brick).
+card(wood).
+card(wheat).
+card(sheep).
 
 
 
 
 
-
-
-
+pushCard([List], [Result|List]) :- 
+  Result is card(_).   
+  
 
 %from stackoverflow - https://stackoverflow.com/questions/15857382/removing-first-occurrence-in-list-prolog.
-delete_one(_, [], []).
+%delete_one(_, [], []).
 delete_one(Term, [Term|Tail], Tail).
 delete_one(Term, [Head|Tail], [Head|Result]) :-
   delete_one(Term, Tail, Result).
 
 % modified from above stackoverflow code.
-delete_two(_, [], []).
-delete_two(Term, [Term|Tail], Result) :-
-	delete_one(Term, [Term|Tail], OutList),
-	delete_one(Term, Outlist, Result).
+%delete_two(_, [], []).
+%delete_two(Term, [Term|Tail], Result) :-
+%	delete_one(Term, [Term|Tail], OutList),
+%	delete_one(Term, Outlist, Result).
 delete_two(Term, [Head|Tail], Result) :-
   delete_one(Term, [Head|Tail], X),
   delete_one(Term, X, Result).
   
   
-sheep_port(_, [], []).
-sheep_port(sheep, [Head|Tail], Result):-
-	delete_two(sheep, [Head|Tail], ResultOfSheepPort),
-        append(Output, ResultOfSheepPort, Result). 
+%sheep_port(_, [], []).
+%sheep_port([Head|Tail], Result):-
+%	delete_two(sheep, [Head|Tail], [ResultOfSheepPort]),
+%        append([card(X)], [ResultOfSheepPort], Result). 
 
+
+sheep_port(QueryVar, [InList]) :-
+    card(QueryVar),
+    delete_two(sheep, InList, InList],
+    InList = [QueryVar | InList].
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+purchase_city([Head|Tail]) :-
+  delete_two(stone, [Head|Tail], _),
+  delete_one(stone, [Head|Tail], _),
+  delete_two(wheat, [Head|Tail], _).
 
 
   
-main_process_loop :- 
-  %10 VP Check
-  %Sheep Port
-  %Purchase City
+has10_VPs([Head|Tail]) :- 
+  10 is aggregate_all(count, makeCities([Head|Tail]).
+
+
+makeCities([Head|Tail]) :-
+  purchase_city([Head|Tail]);
+  sheep_port(sheep, [Head|Tail], [Head|Tail]).
 
   
+*/
   
 %delete_one(_, [], []).
 %delete_one(Term, [Term|Tail], Tail).
